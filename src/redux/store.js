306a18b611e686c649107c976fcfc1bec789df8b
SERGO_MIKIRTUMOV <mikirtumov@gmail.com>
2020-07-02 20:50:27 -0800

    added store.js

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+
+
+const initialState = {};
+const todo = (state = initialState, action) => {
+};
+
+
+const store = createStore(
+    todo
+);
+
+export default store;
