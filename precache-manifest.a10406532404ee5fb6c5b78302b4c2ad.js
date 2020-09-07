823a410df61079cba67e386eeb672bffee896fcc
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-09-06 19:13:55 -0700

    Updates

---
+++ precache-manifest.a10406532404ee5fb6c5b78302b4c2ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551ab5b6c93fd49b496a1f76b4e1e807",
+    "url": "/Redux/index.html"
+  },
+  {
+    "revision": "c7256589bd18e4ffd745",
+    "url": "/Redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38324a0f8ee53c41401e",
+    "url": "/Redux/static/js/2.b98b3003.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Redux/static/js/2.b98b3003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7256589bd18e4ffd745",
+    "url": "/Redux/static/js/main.c54fa20f.chunk.js"
+  },
+  {
+    "revision": "bb83169f3904c9987049",
+    "url": "/Redux/static/js/runtime-main.474b3013.js"
+  }
+]);
